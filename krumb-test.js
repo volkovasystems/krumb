c31fed9df3b69b56d5bc96b38f84577596d5b5b1
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-27 18:45:36 +0800

    Updated test.

--- krumb-test.js
+++ krumb-test.js
@@ -2,8 +2,8 @@
 const assert = require( "assert" );
 const krumb = require( "./krumb.js" );
 
-assert.deepEqual( krumb( { "name": "simple" } ), { "name": "simple" }, "should be equal" );
+assert.deepEqual( krumb( { "name": "simple" } ), { "name": "simple" }, "should be equal to { 'name': 'simple' }" );
 
-assert.deepEqual( krumb( { } ), { }, "should be equal" );
+assert.deepEqual( krumb( { } ), { }, "should be equal to { }" );
 
 console.log( "ok" );
